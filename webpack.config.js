1d0efbfe242c55de9a67197601ff89dba25d503a
Vladyslav Mandziuk <vmandziuk@vivasg.com>
2021-05-18 17:58:36 +0300

    UPDATE codestyle

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,25 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: path.resolve(__dirname, './src/index.js'),
+    entry: path.resolve(__dirname, "./src/index.js"),
     output: {
-      path: path.resolve(__dirname, './dist'),
-      filename: 'bundle.js',
+      path: path.resolve(__dirname, "./dist"),
+      filename: "bundle.js",
     },
     devServer: {
-      contentBase: path.resolve(__dirname, './dist'),
+      contentBase: path.resolve(__dirname, "./dist"),
     },
     module: {
         rules: [
           {
             test: /\.s[ac]ss$/i,
             use: [
-              // Creates `style` nodes from JS strings
+              // Creates `style` nodes from JS strings.
               "style-loader",
-              // Translates CSS into CommonJS
+              // Translates CSS into CommonJS.
               "css-loader",
-              // Compiles Sass to CSS
+              // Compiles Sass to CSS.
               "sass-loader",
             ],
           },
@@ -27,8 +27,8 @@ module.exports = {
       },
     plugins: [
       new HtmlWebpackPlugin({
-        template: path.resolve(__dirname, './src/index.html'),
-        inject: 'body'
+        template: path.resolve(__dirname, "./src/index.html"),
+        inject: "body"
       })
     ],
     devtool: "source-map",  
